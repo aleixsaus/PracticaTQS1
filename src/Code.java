92f10a70c1b69fae16d8391beb86ea8c0cbdf026
Adrián Moreno <adrian.morenog@e-campus.uab.cat>
2020-10-21 19:20:12 +0200

    Creada la clase Code

---
+++ src/Code.java
@@ -0,0 +1,18 @@
+
+public class Code {
+	
+	private String code;
+	
+	public Code() {
+		
+	}
+	
+	public Code(String code) {
+		this.code = code;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+
+}
