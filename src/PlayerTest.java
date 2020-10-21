137f4278e2968c1377671e5fcccb5237e9bbcaac
Cresom <>
2020-10-21 20:22:53 +0200

    Clase runner de tests y clase Player introducción de código y TestPlayer primeros tests realizados.

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -1,4 +1,26 @@
 
+import org.junit.Assert;
+
+import org.junit.Test;
+
 public class PlayerTest {
 
+	public static final int CODE_LENGTH = 4;
+	
+	@Test
+	public void checkPlayerCode() {	
+		Player player = new Player(); 	
+		String playerCode = player.insertCode();
+		
+		String testingCode = "ABCD";
+
+		// Player code its equals working code
+		Assert.assertEquals(playerCode, testingCode);
+		
+		// Player code length is correct
+		Assert.assertTrue(playerCode.length() == CODE_LENGTH);
+		
+		// Player code is not null
+		Assert.assertNotNull(playerCode);	
+	}
 }
